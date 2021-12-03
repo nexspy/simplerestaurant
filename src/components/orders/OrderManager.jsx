97ef25e9861fa5cb0398cf31baee3f0d11dc898e
Number 4 <nirmalmaswa@live.com>
2021-12-03 14:31:32 +0000

    csv export feature added for orders

--- src/components/orders/OrderManager.jsx
+++ src/components/orders/OrderManager.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useHistory } from 'react-router-dom';
+import { CSVLink, CSVDownload } from "react-csv";
 
 import URLS from '../../api/urls';
 import OrderList from './OrderList';
@@ -9,6 +10,7 @@ const OrderManager = () => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [orders, setOrders] = useState([]);
+    const [exportOrders, setExportOrders] = useState([]);
 
     const getOrders = () => {
         const url = URLS.base_url + URLS.order.base;
@@ -20,9 +22,36 @@ const OrderManager = () => {
             });
     }
 
+    const convertToTransaction = () => {
+        var tempOrders = [];
+        orders.map((order) => {
+            var tempOrder = JSON.parse(JSON.stringify(order));
+            if (typeof tempOrder.foods !== 'undefined') {
+                tempOrder.total = 0;
+                tempOrder.foods.map((food) => {
+                    var item = JSON.parse(JSON.stringify(food));
+                    item.id = food._id;
+                    delete item._id;
+                    item.total += food.total;
+                    item.date = order.date;
+                    tempOrders.push(item);
+                });
+            }
+        });
+        return tempOrders;
+    }
+
     useEffect(() => {
         getOrders();
     }, []);
+
+    useEffect(() => {
+        if (orders.length) {
+            var tempOrders = convertToTransaction();
+            setExportOrders(tempOrders);
+            console.log(tempOrders);
+        }
+    }, [orders]);
 
     return (
         <div className="order-manager-wrapper">
@@ -31,7 +60,15 @@ const OrderManager = () => {
             {isLoading ? (
                 <div>loading...</div>
             ) : (
-                <OrderList data={orders} />
+                <div>
+                    <div className="block">
+                        <CSVLink 
+                            data={exportOrders}
+                            filename={ "report-" + Date.now() + ".csv"}
+                        >Export as CSV</CSVLink>
+                    </div>
+                    <OrderList data={orders} />    
+                </div>
             )}
             
         </div>
